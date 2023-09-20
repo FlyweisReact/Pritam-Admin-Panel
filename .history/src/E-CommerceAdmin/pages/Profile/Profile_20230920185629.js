28e5db75ec328165adb5041007a3a739bb1a4a6a
React1 <React1@flyweis.technology>
2023-09-20 19:16:16 +0530

    Changes

--- .history/src/E-CommerceAdmin/pages/Profile/Profile_20230920185629.js
+++ .history/src/E-CommerceAdmin/pages/Profile/Profile_20230920185629.js
@@ -1,186 +1,38 @@
 /** @format */
+
 import React, { useEffect, useState } from "react";
 import HOC from "../../layout/HOC";
-import { Link, useParams } from "react-router-dom";
-import { Form, Button, FloatingLabel ,Spinner } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Form, Button, FloatingLabel, Spinner } from "react-bootstrap";
 import { toast } from "react-toastify";
 import axios from "axios";
 
-const EditProduct = () => {
-  const { id } = useParams();
-  const [name, setName] = useState("");
-  const [categoryId, setCategoryId] = useState(null);
-  const [subcategoryId, setSubCategoryId] = useState(null);
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState(0);
-  const [taxInclude, setTaxInclude] = useState(false);
-  const [tax, setTax] = useState(0);
-  const [discount, setDiscount] = useState(false);
-  const [discountPrice, setDiscountPrice] = useState(0);
-  const [colorActive, setColorActive] = useState(false);
-  const [size, setSize] = useState(null);
-  const [color, setColor] = useState([]);
-  const [colorName, setColorName] = useState("");
-  const [images, setImages] = useState([]);
-  const [image, setImage] = useState(null);
-  const [quantity, setQuantity] = useState(0);
-  const [arrayQuantity, setArrayQuantity] = useState([]);
-  const [quantityDigit, setQuantityDigit] = useState("");
-  const [categoryData, setCategoryData] = useState([]);
-  const [subCategoryData, setSubCategoryData] = useState([]);
-  const [categoryName, setCategoryName] = useState(null);
-  const [ subCatName , setSubCatName] = useState(null)
-  const [costPrice, setCostPrice] = useState(0);
-  const [submitLoading, setSubmitLoading] = useState(false);
+const Profile = () => {
+ const id = localStorage.getItem("AdminId")
+    const [ firstName , setFirstName ] = useState("")
+    const [ lastName , setLastName ] = useState("")
+    const [ phone , setPhone ] = useState("")
+    const [ email , setEmail ] = useState("")
+    const [ password , setPassword ] = useState("")
 
-  const getProductDetail = async () => {
-    try {
-      const res = await axios.get(
-        `https://krish-vapes-backend.vercel.app/api/v1/Product/${id}`
-      );
-      setName(res.data.data.name);
-      setDescription(res.data.data.description);
-      setCostPrice(res.data.data.costPrice);
-      setDiscountPrice(res.data.data.discountPrice);
-      setPrice(res.data.data.price);
-      setTax(res.data.data.tax);
-      setCategoryId(res.data.data?.categoryId?._id);
-      setCategoryName(res.data.data?.categoryId?.name); 
-      setSubCategoryId(res.data.data?.subcategoryId?._id)
-      setSubCatName(res.data.data?.subcategoryId?.name) 
-    } catch (e) {
-      console.log(e);
-      
-    }
-  };
-
-  const getCategory = async () => {
-    try {
-      const res = await axios.get(
-        "https://krish-vapes-backend.vercel.app/api/v1/Category/allCategory"
-      );
-      setCategoryData(res.data.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const getSubCategory = async () => {
-    try {
-      const res = await axios.get(
-        "https://krish-vapes-backend.vercel.app/api/v1/SubCategory/all/SubCategoryForAdmin"
-      );
-      setSubCategoryData(res.data.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    getCategory();
-    getProductDetail();
-    getSubCategory();
-  }, []);
-
-
-  const ColorSelector = (colors) => {
-    setColor((prev) => [...prev, colors]);
-    setColorName("");
-  };
-
-  const RemoveColor = (index) => {
-    setColor((prev) => prev.filter((_, i) => i !== index));
-  };
-
-  const QuantitySelector = (quantity) => {
-    setArrayQuantity((prev) => [...prev, quantity]);
-    setQuantityDigit("");
-  };
-
-  const RemoveQuantity = (index) => {
-    setArrayQuantity((prev) => prev.filter((_, i) => i !== index));
-  };
-
-  const token = localStorage.getItem("AdminToken");
-  const Auth = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
-  const fd = new FormData();
-  fd.append("categoryId", categoryId);
-  fd.append("subcategoryId", subcategoryId);
-  fd.append("name", name);
-  fd.append("description", description);
-  fd.append("price", price);
-  fd.append("taxInclude", taxInclude);
-  fd.append("tax", tax);
-  fd.append("discount", discount);
-  fd.append("discountPrice", discountPrice);
-  fd.append("colorActive", colorActive);
-  fd.append("costPrice", costPrice);
-
-  if (colorActive === "true") {
-    if (size === "false") {
-      fd.append("size", size);
-      Array.from(color).forEach((item) => {
-        fd.append("color", item);
-      });
-      Array.from(images).forEach((img) => {
-        fd.append("images", img);
-      });
-      fd.append("arrayQuantity", arrayQuantity);
-    } else {
-      fd.append("size", size);
-      Array.from(color).forEach((item) => {
-        fd.append("color", item);
-      });
-      Array.from(images).forEach((img) => {
-        fd.append("images", img);
-      });
-    }
-  } else {
-    fd.append("image", image);
-    fd.append("quantity", quantity);
-  }
-
-  const createProduct = async (e) => {
-    e.preventDefault();
-    setSubmitLoading(true);
-    try {
-      const res = await axios.put(
-        `https://krish-vapes-backend.vercel.app/api/v1/Product/editProduct/${id}`,
-        fd,
-        Auth
-      );
-      toast.success(res.data.message);
-      setSubmitLoading(false);
-    } catch (e) {
-      console.log(e);
-      const msg = e.response.data.message;
-      toast.error(msg);
-      setSubmitLoading(false);
-    }
-  };
+ const fetch = async () => {
+    try{
+        const response = await axios.get(`https://pritam-backend.vercel.app/api/v1/admin/getUserById/64a7f34177df0051c3d677ed`)
+        const data = response.data.
+    }catch{}
+ }
 
   return (
     <section>
-      <p className="headP">Dashboard / Edit Product</p>
+      <p className="headP">Dashboard / Crea</p>
       <section className="sectionCont">
         <Form onSubmit={createProduct}>
           <Form.Group className="mb-3">
             <Form.Label>Name</Form.Label>
             <Form.Control
               type="text"
-              value={name}
               onChange={(e) => setName(e.target.value)}
             />
-          </Form.Group>
-
-          <Form.Group className="mb-3">
-            <Form.Label>Previous Category</Form.Label>
-            <Form.Control type="text" defaultValue={categoryName} />
           </Form.Group>
 
           <Form.Group className="mb-3">
@@ -193,11 +45,6 @@ const EditProduct = () => {
                 </option>
               ))}
             </Form.Select>
-          </Form.Group>
-
-          <Form.Group className="mb-3">
-            <Form.Label>Previous Sub-Category</Form.Label>
-            <Form.Control type="text" defaultValue={subCatName} />
           </Form.Group>
 
           <Form.Group className="mb-3">
@@ -218,7 +65,6 @@ const EditProduct = () => {
               <Form.Control
                 as="textarea"
                 style={{ height: "100px" }}
-                value={description}
                 onChange={(e) => setDescription(e.target.value)}
               />
             </FloatingLabel>
@@ -266,7 +112,6 @@ const EditProduct = () => {
                 type="number"
                 min={0}
                 step={0.01}
-                value={tax}
                 onChange={(e) => setTax(e.target.value)}
               />
             </Form.Group>
@@ -293,7 +138,6 @@ const EditProduct = () => {
                 type="number"
                 min={0}
                 step={0.01}
-                value={discountPrice}
                 onChange={(e) => setDiscountPrice(e.target.value)}
               />
             </Form.Group>
@@ -521,7 +365,7 @@ const EditProduct = () => {
 
           <div className="w-100 d-flex justify-content-between">
             <Button variant="success" type="submit">
-            {submitLoading ? (
+              {submitLoading ? (
                 <Spinner animation="border" role="status" />
               ) : (
                 "Submit"
@@ -538,4 +382,4 @@ const EditProduct = () => {
   );
 };
 
-export default HOC(EditProduct);
+export default HOC(Profile);
